29de3b17116d59287ffbe1c69be24a539728e4eb
uveavanto <uve@Yuveshs-MacBook-Air.local>
2016-04-16 18:44:25 +0100

    idk added more stuff and things. a menu?

---
+++ HackAttackFireWorks/Assets/Scripts/globescript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class globescript : MonoBehaviour {
+
+	public int speed =8;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(Vector3.back * Time.deltaTime*speed);
+
+	}
+}
