ad5a6b41378fdd335142a23cbe0a45fa588b2971
uveavanto <uve@Yuveshs-MacBook-Air.local>
2016-04-16 17:53:42 +0100

    omg so many files

---
+++ HackAttackFireWorks/Assets/Scripts/fireWorksHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireWorksHandler : MonoBehaviour {
+
+	public GameObject fireworks;
+	private bool fireworksActive;
+	public ParticleEmitter fire;
+
+	// Use this for initialization
+	void Start () {
+		fireworksActive = false;
+		//fireworks.SetActive(fireworksActive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1"))
+		{
+			fireworksActive = !fireworksActive;
+			//fireworks.SetActive(fireworksActive);
+			fire.emit = fireworksActive;
+		}
+	
+	}
+}
